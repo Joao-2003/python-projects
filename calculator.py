a6061344a365ef67517204a01b81accc790f094a
Joao-2003 <100796909+Joao-2003@users.noreply.github.com>
2023-03-14 11:07:18 -0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,26 @@
+#calculadora simples
+while True:
+    try:
+        a = float(input("Digite o valor do primeiro número:\n"))
+    except:
+        print("Valor inválido: digite apenas valores numéricos inteiros ou decimais com .")
+        continue
+    op = input("Digite a operação que deseja realizar:\n")
+    if op not in '+-*Xx/%':
+        print('Operação inválida, digite apenas os operadores +, -, *, /, ou ainda x e % para multiplicação e divisão.\n')
+        continue
+    try:
+        b = float(input("Digite o valor do segundo número:\n"))
+    except:
+        print("Valor inválido: digite apenas valores numéricos inteiros ou decimais com .")
+        continue
+    if op == '+':
+        print("A soma de {} com {} é {}." .format(a,b,a+b))
+    elif op == '-':
+        print("A subtração de {} com {} é {}." .format(a,b,a-b))
+    elif op == '*' or op == 'x':
+        print("A multiplicação de {} por {} é {}." .format(a,b,a*b))
+    elif op == '/' or op == '%':
+        print("A divisão de {} por {} é {}." .format(a,b,a/b))
+    else:
+        print("Operação inválida.\n")
